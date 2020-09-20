a086af8aebfb76e515763774945808c2bea9ae3d
Nodragem <geoffrey.megardon@outlook.com>
2020-09-20 19:38:43 +0100

    force changes

--- pushToRemoteServer.sh
+++ pushToRemoteServer.sh
@@ -22,4 +22,4 @@ echo "Generating site"
 hugo --theme=hugo-academic
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages `date`" && git push origin gh-pages
+cd public && git add --all && git commit -m "Publishing to gh-pages `date`" && git push origin gh-pages -f
